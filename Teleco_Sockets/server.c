9cdc218a6c156b9f95840592849a61faf2a01ee9
Oscar <oscargallego27@gmail.com>
2018-11-15 20:00:11 -0500

    Se refactoriza codigo y se soluciona error del user

--- Teleco_Sockets/server.c
+++ Teleco_Sockets/server.c
@@ -20,37 +20,45 @@ void err_quit(char *msg)
     exit(EXIT_FAILURE);
 }
 
+
+/*
+Developer: DanielToro05 - Hinoga
+Description: Funcion que realiza la logica de los comandos
+Parameter: char *command, int srcfd, int cnt
+Return: void
+Date: 15/11/2018
+*/
+void commandMenu(char *command, int srcfd, int cnt)
+{
+	int buffTmp = 255;
+    if (strcmp ("-user", command)==0){
+       char user [10] = "daniel"; // se predefine un usuario
+			char sendcommand[15] = "Type user:"; //string que se envia al cliente
+        if(send(srcfd,sendcommand,buffTmp,0) == ERROR) //envía la string al cliente mediante send
+                perror("Error al enviar bienvenida usuario\n");	 //si hay algun error, informa
+        while((cnt = read(srcfd, command, sizeof(command))) > 0){ //lee lo que manda el cliente
+            if (strcmp(command,user)== 0) { //compara la informacion del cliente a conectar con el usuario predefinido 
+                if(send(srcfd,"Connected\n",buffTmp,0) == ERROR){ //Envia al cliente la informacion de que se conectó
+                    perror("Error al enviar bienvenida usuario\n");	//si hay algun error, informa
+                }
+                break;
+            }
+        }
+    } else if (strcmp (command,"-ls")==0) {
+        printf("el if funciona\n");	
+		system("ls > temporal.txt");
+        //if(send(srcfd,fileBuffer,LINE_SIZE,0) == ERROR)
+        //perror("Error al enviar el comando\n");	
+    }
+}
+
 void xfer_data(int srcfd, int tgtfd, int sockfd)
 {	
-	char buf[80];
-	char command1[80] = "-ls"; //se predefine el comando -ls
-	char command2[80] = "-user"; //se predefine el comando -user
-	char user2[15] = "Connected\n"; //se predefine el comando a enviar
-	int cnt, len;
-	int buffTmp = 255;
-	
-
-    // Leer de la entrada fd y escribir en la salida fd
+    char buf[80];
+    int cnt;
+    // Leer de la entr char buf[80];ada fd y escribir en la salida fd
     while((cnt = read(srcfd, buf, sizeof(buf))) > 0){
-		if (strcmp(buf,command1)== 0){
-			printf("el if funciona\n");	
-			system("ls > temporal.txt");
-				//if(send(srcfd,fileBuffer,LINE_SIZE,0) == ERROR)
-					//perror("Error al enviar el comando\n");	
-		}
-		else if (strcmp(buf,command2)==0){ //Compara si el comando enviado es igual a-user
-			char user [10] = "daniel"; // se predefine un usuario
-			char sendcommand[15] = "Type user:"; //string que se envia al cliente
-			if(send(srcfd,sendcommand,buffTmp,0) == ERROR) //envía la string al cliente mediante send
-					perror("Error al enviar bienvenida usuario\n");	 //si hay algun error, informa
-			while((cnt = read(srcfd, buf, sizeof(buf))) > 0){ //lee lo que manda el cliente
-				if (strcmp(buf,user)== 0) { //compara la informacion del cliente a conectar con el usuario predefinido 
-					if(send(srcfd,user2,buffTmp,0) == ERROR){ //Envia al cliente la informacion de que se conectó
-						perror("Error al enviar bienvenida usuario\n");	//si hay algun error, informa
-					}
-				}
-			}
-		}
+        commandMenu(buf, srcfd, cnt);
 	}
 }
 
